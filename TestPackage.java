99106b05c0f2da982c930a75ce01c2dc649745bd
Suraj Verma <surajverma8730448@gmail.com>
2024-03-04 20:53:51 +0530

    package tested

--- TestPackage.java
+++ TestPackage.java
@@ -1,10 +1,15 @@
-import myPackage.TestClass1;
+import myPackage.*;
 import myPackage.TestClass2;
 
 public class TestPackage{
     public static void main(String[] args) {
         TestClass1 tc1 = new TestClass1();
         TestClass2 tc2 = new TestClass2();
+
+        /*
+        NOTE: as a java file is has only one public class so other classes arenot visible outside the package
+         */
+        // InnerTestClass1 itc1 = new InnerTestClass1();
 
         tc1.displayName("Suraj");
         tc2.displayName("Suraj");
--- myPackage/TestClass1.java
+++ myPackage/TestClass1.java
@@ -4,4 +4,15 @@ public class TestClass1{
     public void displayName(String name){
         System.out.println("Hi, " + name);
     }
+
+    public static void main(String[] args) {
+        InnerTestClass1 obj = new InnerTestClass1();
+        obj.displayAge(20);
+    }
+}
+
+class InnerTestClass1 {
+    public void displayAge(int age){
+        System.out.println("Your age is : " + age);
+    }
 }
