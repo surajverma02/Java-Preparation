aca9f8d889c8a7b8a4d827a82226f349074e0493
Suraj Verma <surajverma8730448@gmail.com>
2024-03-09 19:21:30 +0530

    something gone wrong after changing the code output not changes

--- multithreading/ThreadMethods.java
+++ multithreading/ThreadMethods.java
@@ -4,20 +4,15 @@ import java.lang.Thread;
 class ThreadA extends Thread{
     public void run(){
 
-        for(int i=1; i<=5; i++){
-            if (i==1) {
-                // yield();
-            }
-            System.out.println("\tFrom Thread A : i = " + i);
-        }
-        System.out.println("Exit from A ");
+        System.out.println(Thread.currentThread());
+        System.out.println("HI");
     }
 }
 
 class ThreadB extends Thread{
     public void run(){
 
-        for(int j=1; j<=5; j++){
+        for(int j=1; j<=3; j++){
             System.out.println("\tFrom Thread B : j = " + j);
             if(j==3){
                 // stop();
@@ -30,7 +25,7 @@ class ThreadB extends Thread{
 class ThreadC extends Thread{
     public void run(){
 
-        for(int k=1; k<=5; k++){
+        for(int k=1; k<=3; k++){
             System.out.println("\tFrom Thread C : k = " + k);
             if (k==2) {
                 try{
@@ -54,10 +49,17 @@ public class ThreadMethods {
         ThreadC C = new ThreadC();
 
         System.out.println("Strating Thread A");
+        System.out.println(A.getState());
         A.start();
+        System.out.println(A.getState());
         System.out.println("Strating Thread B");
+        System.out.println(B.getState());
         B.start();
+        System.out.println(B.getState());
         System.out.println("Strating Thread C");
+        System.out.println(C.getState());
         C.start();
+        System.out.println(C.getState());
+        System.out.println(Thread.currentThread());
     }
 }
